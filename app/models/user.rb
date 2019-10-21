42fcdc8e97c08347fc5f56e431aa4a13e2ee1d06
danielahn <93danielahn@gmail.com>
2019-10-21 16:04:01 -0400

    Add JWT auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :budget_lists
     has_many :budget_items, through: :budget_lists
+    has_secure_password
+    validates :username, uniqueness: true, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,18 @@ BudgetItem.destroy_all
 
 
 puts "test"
-user_a = User.create!(username: "Darrow")
-user_c = User.create!(username: "Daniel")
-user_b = User.create!(username: "Belinda")
+user_a = User.create!(username: "Darrow", password: "123456")
 puts "done making users"
 
 
 list_a = BudgetList.create!(title: "moving out", total_budget: 200, user: user_a)
-list_b = BudgetList.create!(title: "eating fries", total_budget: 5, user: user_b)
-list_c = BudgetList.create!(title: "hiring a clown", total_budget: 500, user: user_c)
+# list_b = BudgetList.create!(title: "eating fries", total_budget: 5, user: user_b)
+# list_c = BudgetList.create!(title: "hiring a clown", total_budget: 500, user: user_c)
 puts "done making lists"
 
  
 item_a = BudgetItem.create!(name: "uhaul", price: 200, budget_list: list_a, paid_for: false)
-item_b = BudgetItem.create!(name: "curly fries", price: 5, budget_list: list_b, paid_for: false)
-item_c = BudgetItem.create!(name: "a killer clown", price: 500, budget_list: list_c, paid_for: false)
+# item_b = BudgetItem.create!(name: "curly fries", price: 5, budget_list: list_b, paid_for: false)
+# item_c = BudgetItem.create!(name: "a killer clown", price: 500, budget_list: list_c, paid_for: false)
 puts "done making items"
 
