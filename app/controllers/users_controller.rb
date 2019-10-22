c0fd3907f8d85da6e866cff1d418c6e6123deb25
danielahn <93danielahn@gmail.com>
2019-10-22 10:52:45 -0400

    Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,25 @@
 class UsersController < ApplicationController
 
-    def create
-        user = User.new(user_params)
-        if user.save
-            render json: user
-        else 
-            render json: {errors: user.errors.full_messages}
-        end
+  skip_before_action :authorized, only: [:create]
 
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
+  end
 
-    # do we need this method?
-    def show
-        user = User.find(params[:id])
-        render json: user
-    end
+  private
 
-    def destroy
-        user = User.find(params[:id])
-        user.destroy
-        render json: {message: "Successfully Deleted"}
-    end
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 
-    private
-
-    def user_params
-        params.require(:user).permit(:username)
-    end
 end
